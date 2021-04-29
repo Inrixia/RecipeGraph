6a54af3acf1a4fe79ad90e554dea8898540614ea
Inrix <Inrixia@gmail.com>
2021-04-30 10:46:58 +1200

    Added image for reference & nodesSeen

--- lib/RecipeGraph.ts
+++ lib/RecipeGraph.ts
@@ -3,6 +3,7 @@ import { Recipe, RecipeObj } from "./Recipe";
 import { Resource } from "./Resource";
 
 export type NestedArray<T> = Array<T | NestedArray<T>>
+export type Path = { weight: NestedArray<number> | number, nodesSeen: Array<string|undefined> };
 export class RecipeGraph {
 	public readonly recipes: Array<Recipe>;
 	public readonly resources: Record<string, Resource> = {}
@@ -16,18 +17,18 @@ export class RecipeGraph {
 		return this.resources[resourceName] = new Resource(resourceName);
 	}
 
-	public costToMake = (resource: string, amountWanted: number): NestedArray<number> | number => {
+	public costToMake = (resource: string, amountWanted: number): NestedArray<Path> | Path => {
 		const startNode = this.resources[resource];
 		if (startNode === undefined) throw Error("startNode does not exist!");
 		
 
 		
-		const iteratePaths = (node: GraphNode, weight: number): NestedArray<number> | number => {
-			if (node.regressiveEdges.length === 1) return iteratePaths(node.regressiveEdges[0].toNode, weight*node.regressiveEdges[0].weight);
-			else if (node.regressiveEdges.length === 0) return weight;
+		const iteratePaths = (node: GraphNode, weight: number, nodesSeen: Path["nodesSeen"] = []): NestedArray<Path> | Path => {
+			if (node.regressiveEdges.length === 1) return iteratePaths(node.regressiveEdges[0].toNode, weight*node.regressiveEdges[0].weight, nodesSeen);
+			else if (node.regressiveEdges.length === 0) return { weight, nodesSeen };
 
 			return node.regressiveEdges.map(edge => {
-				return iteratePaths(edge.toNode, weight*(edge.weight));
+				return iteratePaths(edge.toNode, weight*(edge.weight), [...nodesSeen]);
 			});
 		};
 
